a01972ee7a68c63a7c24c76f64c7bc280e8dfae9
dntzhang <kmdjs@qq.com>
2017-12-28 11:58:02 +0800

    update omi file

---
+++ template/x/src/component/vote-list/index.js
@@ -0,0 +1,63 @@
+
+
+class VoteList extends Omi.Component {
+    constructor(data, option){
+        super(data, option)
+    }
+
+    style(){
+        return require('./_index.less')
+    }
+
+    showProfile(uin) {
+        mqq.ui.showProfile({
+            uin: uin + "",
+            uinType: 0
+        })
+    }
+
+    render() {
+
+        let list = this.data.left?this.$store.listA:this.$store.listB
+
+        return (
+            <div class="vote-list" style={"left:"+(this.data.left?"3%":"53%")}>
+                <div ref="scroller">
+                    {
+                        list.map(item =>
+                                <div class="vote-item" onclick={()=>{this.showProfile(item.user_info.uin)}}>
+                                    <img src={item.user_info.portrait} />
+                                    <div class={item.source==1?'right single':'right'}>
+                                        <div class="l1">{item.user_info.nick}</div>
+                                        <div class='l2'>{this.getTextBySource(item.source)}</div>
+                                    </div>
+                                </div>
+                        )
+                    }
+
+                </div>
+            </div>
+        )
+    }
+
+    getTextBySource(source) {
+        return ['来自手机QQ','','来自微信'][source]
+    }
+}
+
+//<div class="vote-item">
+//    <img src="https://p.qpic.cn/qqconadmin/0/b61f2670aa8a4dab91cccbbdde54c510/0" />
+//    <div class="right">
+//        <div class="l1">小猪他大家撒反对数据库很忙</div>
+//        <div class='l2'>来自手机QQ</div>
+//    </div>
+//</div>
+//<div class="vote-item">
+//    <img src="https://p.qpic.cn/qqconadmin/0/b61f2670aa8a4dab91cccbbdde54c510/0" />
+//    <div class="right single">
+//    <div  class="l1">小猪他很忙</div>
+//    </div>
+//    </div>
+
+
+export default VoteList
--- template/x/src/page/index/main.js
+++ template/x/src/page/index/main.js
@@ -2,7 +2,33 @@ import './container';
 
 if ("production" !== process.env.NODE_ENV) {
 
-    require('vconsole/dist/vconsole.min.js');
+   // require('vconsole/dist/vconsole.min.js');
     // use it for hot reload
     module.hot.accept();
 }
+
+
+
+
+import AppStore from './store.js'
+import App from './container'
+let app = new App()
+
+let store =  new AppStore({
+    onInit:()=>{
+        app.update()
+    },
+    onLoadMore:()=>{
+        app.update()
+    },
+    onListInit:()=>{
+        app.update()
+    }
+})
+
+Omi.render(app, '#pages',{
+    store
+})
+
+
+store.init()
