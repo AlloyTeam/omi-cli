1d078927de467a075e2e48e10b777ba4e49d8d2f
dntzhang <mhtml5@qq.com>
2018-05-02 17:23:21 +0800

    fix hot reload

--- template/app/src/page/index/main.js
+++ template/app/src/page/index/main.js
@@ -2,13 +2,18 @@ import './container';
 
 if ("production" !== process.env.NODE_ENV) {
 
-   // require('vconsole/dist/vconsole.min.js');
     // use it for hot reload
     module.hot.accept();
+
+	// dispose handler
+	module.hot.dispose(function() {
+		// revoke the side effect
+        document.querySelector('#pages').innerHTML = ' '
+	});
 }
 
 
-
+ 
 
 import AppStore from './store.js'
 import App from './container'
--- template/app/src/page/page-b/main.js
+++ template/app/src/page/page-b/main.js
@@ -2,7 +2,12 @@ import './container';
 
 if ("production" !== process.env.NODE_ENV) {
 
-    require('vconsole/dist/vconsole.min.js');
     // use it for hot reload
     module.hot.accept();
+
+    // dispose handler
+	module.hot.dispose(function() {
+		// revoke the side effect
+        document.querySelector('#pages').innerHTML = ' '
+	});
 }
