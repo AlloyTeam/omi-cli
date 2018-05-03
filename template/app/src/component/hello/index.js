a5ab9c32af08823758f788a3a41eabf75c5c1a52
dntzhang <mhtml5@qq.com>
2018-05-03 10:18:13 +0800

    update template

--- template/app/src/component/hello/index.js
+++ template/app/src/component/hello/index.js
@@ -5,13 +5,16 @@ import Intro from '../intro/index.js'
 class Hello extends Omi.Component {
   constructor(data, option){
       super(data, option)
+      this.handleClick = this.handleClick.bind(this)
   }
 
   handleClick(){
       this.$store.rename('Hello Omi !')
+
       import("./a.js").then(function(moduleA) {
-          //console.log(moduleA);
-          document.body.innerHTML+=`<img src="${moduleA.getSrc()}">`
+          let img = document.createElement('img')
+          img.src = moduleA.getSrc()
+          document.body.appendChild(img)
       });
   }
 
@@ -23,7 +26,7 @@ class Hello extends Omi.Component {
 
     return <div class="app">
                 <div class="app-header">
-                  <img src={logo} onclick={this.handleClick.bind(this)} class="app-logo" alt="logo" />
+                  <img src={logo} onclick={this.handleClick} class="app-logo" alt="logo" />
                   <h2>Welcome to Omi</h2>
                 </div>
                 <Intro></Intro>
