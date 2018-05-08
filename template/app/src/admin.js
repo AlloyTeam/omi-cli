e5794f7a5cf8990a41104e2149c74b34e5648b6f
当耐特 <kmdjs@qq.com>
2018-05-08 21:57:20 +0800

    Update admin.js

--- template/app/src/admin.js
+++ template/app/src/admin.js
@@ -1,17 +1,17 @@
 import Omi from 'omi'
-import Nav from './components/hello'
+import Hello from './components/hello'
 import registerServiceWorker from './assets/register-service-worker'
 
 
 import AdminStore from './store/admin-store'
   
-const nav = new Nav()
+const hello = new Hello()
 const adminStore = new AdminStore({ name: 'Omi' }, {
   onRename: () => {
-    nav.update()
+    hello.update()
   }
 })
 
 
-Omi.render(nav,'#root',adminStore)
+Omi.render(hello, '#root', adminStore)
 registerServiceWorker()
