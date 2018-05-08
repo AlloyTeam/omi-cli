97ba48030d072bd690a3c3ac72e3fb5a3aaba04e
当耐特 <kmdjs@qq.com>
2018-05-08 21:58:48 +0800

    Update admin.js

--- template/app/src/admin.js
+++ template/app/src/admin.js
@@ -1,8 +1,6 @@
 import Omi from 'omi'
 import Hello from './components/hello'
 import registerServiceWorker from './assets/register-service-worker'
-
-
 import AdminStore from './store/admin-store'
   
 const hello = new Hello()
